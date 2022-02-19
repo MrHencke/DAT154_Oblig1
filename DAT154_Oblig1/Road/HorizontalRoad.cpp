65df1bae824eeedc89a7a0b8351fbbeb4057e7bf
MrHencke <henckedev@outlook.com>
2022-02-19 02:45:29 +0100

    Update lane separator colors

--- DAT154_Oblig1/Road/HorizontalRoad.cpp
+++ DAT154_Oblig1/Road/HorizontalRoad.cpp
@@ -5,16 +5,16 @@
 
 void HorizontalRoad::drawLaneDivider(HDC hdc) {
     Road::draw(hdc);
+    for (int i = 1; i < road_lanes; i++) {
     LOGBRUSH lb;
-    lb.lbColor = col_yellow;
+    lb.lbColor = i == road_lanes/2 ? col_yellow : col_white;
     lb.lbStyle = PS_SOLID;
     HGDIOBJ hPen = ExtCreatePen(PS_GEOMETRIC | PS_DASH, 2, &lb, 0, NULL);
     HGDIOBJ hPenOrg = SelectObject(hdc, hPen);
-    for (int i = 1; i < road_lanes; i++) {
         MoveToEx(hdc, west_pos, i * lane_width + north_pos, NULL);
         LineTo(hdc, east_pos, i * lane_width + north_pos);
-    }
     SelectObject(hdc, hPenOrg);
     DeleteObject(hPen);
     DeleteObject(hPenOrg);
+    }
 }
--- DAT154_Oblig1/Road/VerticalRoad.cpp
+++ DAT154_Oblig1/Road/VerticalRoad.cpp
@@ -3,18 +3,16 @@
 
 void VerticalRoad::drawLaneDivider(HDC hdc) {
     Road::draw(hdc);
+    for (int i = 1; i < road_lanes; i++) {
     LOGBRUSH lb;
-    lb.lbColor = col_yellow;
+    lb.lbColor = i == road_lanes / 2 ? col_yellow : col_white;
     lb.lbStyle = PS_SOLID;
     HGDIOBJ hPen = ExtCreatePen(PS_GEOMETRIC | PS_DASH, 2, &lb, 0, NULL);
     HGDIOBJ hPenOrg = SelectObject(hdc, hPen);
-
-    for (int i = 1; i < road_lanes; i++) {
         MoveToEx(hdc, i * lane_width + west_pos, north_pos, NULL);
         LineTo(hdc, i * lane_width + west_pos, south_pos);
-    }
-
     SelectObject(hdc, hPenOrg);
     DeleteObject(hPen);
     DeleteObject(hPenOrg);
+    }
 }
